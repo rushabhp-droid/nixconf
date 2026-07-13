5e72c52720445795ef1ce43ace05751e69f7c2a3
Rushabh Patil <rushabhpatil3407@gmail.com>
2026-07-13 10:04:27 +0530

    format via treefmt

--- modules/features/nixos/hardware/librepods.nix
+++ modules/features/nixos/hardware/librepods.nix
@@ -1,6 +1,6 @@
-{ self, inputs, ... }: {
-  flake.nixosModules.librepods = { pkgs, ... }: {
-    nixpkgs.overlays = [self.overlays.librepods];
+{ self, ... }: {
+  flake.nixosModules.librepods = _: {
+    nixpkgs.overlays = [ self.overlays.librepods ];
     programs.librepods.enable = true;
   };
 }
--- modules/overlays/librepods.nix
+++ modules/overlays/librepods.nix
@@ -1,10 +1,16 @@
-{ self, inputs, ... }: {
+_: {
   flake.overlays.librepods = _final: prev: {
     librepods = prev.librepods.overrideAttrs (old: {
-      qtWrapperArgs = (old.qtWrapperArgs or []) ++ [
-        "--set" "QT_STYLE_OVERRIDE" "Fusion"
-        "--set" "QT_QUICK_CONTROLS_STYLE" "Fusion"
-        "--set" "WGPU_BACKEND" "vulkan"
+      qtWrapperArgs = (old.qtWrapperArgs or [ ]) ++ [
+        "--set"
+        "QT_STYLE_OVERRIDE"
+        "Fusion"
+        "--set"
+        "QT_QUICK_CONTROLS_STYLE"
+        "Fusion"
+        "--set"
+        "WGPU_BACKEND"
+        "vulkan"
       ];
     });
   };
