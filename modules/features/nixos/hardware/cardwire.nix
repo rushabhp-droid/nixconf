96131e05c599d3073f0d00f64addf6eec1fc72a7
Rushabh Patil <rushabhpatil3407@gmail.com>
2026-07-19 16:19:53 +0530

    changed cardwire auto mode to hybrid

--- modules/features/nixos/hardware/cardwire.nix
+++ modules/features/nixos/hardware/cardwire.nix
@@ -7,7 +7,7 @@
         auto_apply_gpu_state = true;
         experimental_nvidia_block = true;
         battery_auto_switch = true;
-        battery_auto_switch_mode = "integrated";
+        battery_auto_switch_mode = "hybrid";
       };
     };
   };
